1faa7585327f9bd922c2de7cbef121c5db6dab46
Craig Paton <craigpaton@gmail.com>
2025-12-05 18:52:22 +0000

    feat(powershell): bring back all hosts profile

--- dot_config/powershell/profile.include.zoxide.ps1
+++ dot_config/powershell/profile.include.zoxide.ps1
@@ -1,6 +1,7 @@
 . __ProfileCachedInitialization "zoxide" {
-    if ($null -ne (Get-Command zoxide -ErrorAction SilentlyContinue)) {
-        zoxide init powershell --no-cmd | Out-String
+    if ($null -ne (Get-Command zoxide -ErrorAction SilentlyContinue))
+    {
+        zoxide init powershell | Out-String
         # zoxide init powershell --no-cmd | Out-String
     }
 }
---
+++ dot_config/powershell/profile.ps1
@@ -0,0 +1,1 @@
+. ( Join-Path $PSScriptRoot "profile.all-hosts.ps1" )
