e3adba5e800724baa2756485c0d3fa161b79e3b0
Craig Paton <craigpaton@gmail.com>
2026-01-06 14:56:55 +0000

    feat(powershell): Set minimum height for fzf for tab completion

--- dot_config/powershell/fuzzy-tab-expansion.ps1
+++ dot_config/powershell/fuzzy-tab-expansion.ps1
@@ -29,7 +29,8 @@
 .SYNOPSIS
 Wraps TabExpansion2 to provide fuzzy selection via fzf when multiple completions are available.
 #>
-function Invoke-FuzzyTabExpansion2 {
+function Invoke-FuzzyTabExpansion2
+{
     [CmdletBinding()]
     param()
 
@@ -46,16 +47,19 @@ function Invoke-FuzzyTabExpansion2 {
     $completion = TabExpansion2 $line $cursor
 
     $completionMatches = $completion.CompletionMatches
-    if (-not $completionMatches -or $completionMatches.Count -eq 0) {
+    if (-not $completionMatches -or $completionMatches.Count -eq 0)
+    {
         return
     }
 
-    if ($completionMatches.Count -eq 1) {
+    if ($completionMatches.Count -eq 1)
+    {
         $choice = $completionMatches[0].CompletionText
-    }
-    else {
-        $choice = $completionMatches.CompletionText | fzf "--height=~$($completionMatches.Count)" --min-height=5 --reverse --prompt="$line> "
-        if (-not $choice) {
+    } else
+    {
+        $choice = $completionMatches.CompletionText | fzf "--height=~$([Math]::Max(10, $completionMatches.Count))" --min-height=10 --reverse --prompt="$line> "
+        if (-not $choice)
+        {
             return
         }
     }
@@ -79,10 +83,11 @@ function Invoke-FuzzyTabExpansion2 {
     # [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($newCursor)
 }
 
-if ($IsLinux) {
+if ($IsLinux)
+{
     Set-PSReadLineKeyHandler -Key Ctrl+Spacebar -ScriptBlock { Invoke-FuzzyTabExpansion2 } -BriefDescription "Fuzzy Tab Completion" -Description "Invoke Fuzzy Tab Completion using fzf"
-}
-else {
+} else
+{
     # On windows when run through PSReadline keyhandler input and output is redirected
     # This means fzf defaults to its fallback behavior and uses the full screen which can be a jarring experience
     # So use alternative keybinding to offer both experiences
