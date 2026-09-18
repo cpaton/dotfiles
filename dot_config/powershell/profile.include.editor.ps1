b1e25a4375fbd11fe107f323fc3e4e3eecb815d1
Craig Paton <craigpaton@gmail.com>
2026-09-18 19:39:43 +0100

    feat(powershell): Load mise earlier in sequence so nvim comes from mise

--- dot_config/powershell/profile.include.editor.ps1
+++ dot_config/powershell/profile.include.editor.ps1
@@ -1,16 +1,20 @@
-# NeoVim
-if (Test-Path C:\_cp\apps\nvim\bin\nvim.exe) {
-    New-Alias -Name v -Value C:\_cp\apps\nvim\bin\nvim.exe
-    New-Alias -Name vi -Value C:\_cp\apps\nvim\bin\nvim.exe
-    New-Alias -Name vim -Value C:\_cp\apps\nvim\bin\nvim.exe
-    New-Alias -Name nvim -Value C:\_cp\apps\nvim\bin\nvim.exe
-}
-
 if (Test-Path 'C:\_cp\apps\npp.8.3.3.portable.x64\notepad++.exe') {
     New-Alias -Name npp -Value 'C:\_cp\apps\npp.8.3.3.portable.x64\notepad++.exe'
 }
 
-if ($null -ne (Get-Command nvim -ErrorAction SilentlyContinue)) {
+# NeoVim
+$neoVimCommand = Get-Command nvim -ErrorAction SilentlyContinue
+if ($null -ne $neoVimCommand) {
     $env:VISUAL = 'nvim'
     $env:EDITOR = 'nvim'
+    New-Alias -Name v -Value $neoVimCommand.Source
+    New-Alias -Name vi -Value $neoVimCommand.Source
+    New-Alias -Name vim -Value $neoVimCommand.Source
+    New-Alias -Name nvim -Value $neoVimCommand.Source
+}
+elseif (Test-Path C:\_cp\apps\nvim\bin\nvim.exe) {
+    New-Alias -Name v -Value C:\_cp\apps\nvim\bin\nvim.exe
+    New-Alias -Name vi -Value C:\_cp\apps\nvim\bin\nvim.exe
+    New-Alias -Name vim -Value C:\_cp\apps\nvim\bin\nvim.exe
+    New-Alias -Name nvim -Value C:\_cp\apps\nvim\bin\nvim.exe
 }
